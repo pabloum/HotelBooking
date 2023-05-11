e780ac77a557934b79a6ca5a60851a7f2343f15f
Pablo Uribe <pablo.um00@gmail.com>
2023-05-10 22:38:26 -0500

    Implement some validations

--- HotelBooking.Services/Services/ReservationValidationService.cs
+++ HotelBooking.Services/Services/ReservationValidationService.cs
@@ -12,22 +12,22 @@ namespace HotelBooking.Services.Services
 
 		public bool IsReservationPossible(Room room)
 		{
-			if (!AreDatesAvailable())
+			if (!AreDatesAvailable(room.StartReservation, room.EndReservation))
 			{
 				return false;
 			}
 
-			if (!IsReservationWithLess30DaysInAdvance())
+			if (!IsReservationWithLess30DaysInAdvance(room.StartReservation))
 			{
 				return false;
 			}
 
-			if (!IsReservationLessThan3Days())
+			if (!IsReservationLessThan3Days(room.StartReservation, room.EndReservation))
 			{
 				return false;
 			}
 
-            if (!IsReservatioForAtLeastNextDayOfBooking())
+            if (!IsReservatioForAtLeastNextDayOfBooking(room.StartReservation))
             {
                 return false;
             }
@@ -35,23 +35,42 @@ namespace HotelBooking.Services.Services
             return true;
 		}
 
-		private bool AreDatesAvailable()
+		private bool AreDatesAvailable(DateTime startDate, DateTime endDate)
 		{
 			return true;
 		}
 
-		private bool IsReservationWithLess30DaysInAdvance()
+		private bool IsReservationWithLess30DaysInAdvance(DateTime startDate)
 		{
-			return true;
+            var currentDate = DateTime.Now;
+
+			if (currentDate.AddDays(30).Date > startDate.Date)
+			{
+				return false;
+			}
+
+            return true;
 		}
 
-		private bool IsReservationLessThan3Days()
+		private bool IsReservationLessThan3Days(DateTime startDate, DateTime endDate)
         {
+            if (endDate - startDate <= new TimeSpan(3,0,0,0))
+            {
+                return false;
+            }
+
             return true;
         }
 
-        private bool IsReservatioForAtLeastNextDayOfBooking()
+        private bool IsReservatioForAtLeastNextDayOfBooking(DateTime startDate)
         {
+            var currentDate = DateTime.Now;
+
+            if (startDate > currentDate.AddDays(1))
+			{
+				return false;
+			}
+
             return true;
         }
     }
