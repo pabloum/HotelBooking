17f21afe0d81556a0ef374d027c838a667e02f4f
Pablo Uribe <pablo.um00@gmail.com>
2023-05-13 21:39:42 -0500

    Fix failing test

--- HotelBooking.Tests/DataFactory/DataFactory.cs
+++ HotelBooking.Tests/DataFactory/DataFactory.cs
@@ -8,7 +8,7 @@ namespace HotelBooking.Tests.DataFactory
 		private static IEnumerable<Room> _rooms = new List<Room>
 		{
 			new Room{ RoomId = 1, StartReservation = DateTime.Parse("2023-08-09"), EndReservation = DateTime.Parse("2023-08-19")},
-			new Room{ RoomId = 2, StartReservation = DateTime.Parse("2023-09-09"), EndReservation = DateTime.Parse("2023-09-19")},
+			new Room{ RoomId = 2, StartReservation = DateTime.Parse("2023-09-09"), EndReservation = DateTime.Parse("2023-09-18")},
 			new Room{ RoomId = 3, StartReservation = DateTime.Parse("2023-10-09"), EndReservation = DateTime.Parse("2023-10-19")},
         };
 
